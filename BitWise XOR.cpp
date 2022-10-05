af6b1c38d990a820f1d1ee72fd1c7c1c82d2ea25
Ankit Maddheshiya <amddheshiya637@gmail.com>
2022-10-05 14:18:51 +0530

    BitWise XOR

---
+++ BitWise XOR.cpp
@@ -0,0 +1,14 @@
+// Leetcode 2425. Bitwise XOR of All Pairings
+
+int xorAllNums(vector<int> &A, vector<int> &B)
+{
+    int x = 0, y = 0;
+    for (int a : A)
+        x ^= a;
+    for (int b : B)
+        y ^= b;
+    return (A.size() % 2 * y) ^ (B.size() % 2 * x);
+}
+
+// Time Complexity  ::  O(n)
+// Space Complexity :: O(1)
